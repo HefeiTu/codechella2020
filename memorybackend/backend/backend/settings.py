bb7f8a867c313ebff073140bfc3590fc13d58bf5
Zhiheng Jiao <zhihengj@andrew.cmu.edu>
2020-11-22 02:25:47 -0500

    push new

--- memorybackend/backend/backend/settings.py
+++ memorybackend/backend/backend/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
+    'rest_framework',
+    'firstapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +52,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'backend.urls'
 
--- memorybackend/backend/firstapp/views.py
+++ memorybackend/backend/firstapp/views.py
@@ -1,6 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Applicant
+from .models import Comment
 
 from django.shortcuts import render
 
 # Create your views here.
+@api_view(['GET'])
+def get_applicant(request):
+    contact = request.query_params['contact']
+    entrys = Applicant.objects.filter(contact=contact)
+    res = 0
+    if len(entrys) > 0:
+        res = 1
+    return Response(res)
+
+
+@api_view(['POST'])
+def add_applicant(request):
+    res = request.data
+    entry = Applicant(
+        name = res['name'],
+        education = res['education'],
+        contact = res['contact'],
+        status = 'Applied',
+        rate = 0,
+        rate_number = 0,
+        comment=[]
+    )
+    entry.save()
+    return Response(res)
